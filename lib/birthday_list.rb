74341975a285e63e39c14ebcfa893e4c61edb737
Jordan <jordanmanu@live.com.au>
2022-03-17 14:46:25 +0000

    First commit

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,25 @@
+class Birthday
+  def initialize
+    @birthday_list = {
+      Mary: "May 31st, 1993",
+      Mitchell: "June 15th, 1993",
+      Kyle: "Jan 27th, 1986", 
+      Sally: "March 17th, 1966"
+    }
+    @todays_date = Time.new
+  end
+  def birthday_list
+    @birthday_list
+  end
+  def display_birthdays
+    @birthday_list.map { |k,v| "#{k}, #{v}\n" }.join("")
+  end   
+  def todays_birthdays
+    date = @todays_date.strftime("%B %dth")
+    @birthday_list.values.include?(date) ? "Yes" : "No"
+    # @birthday_list.map { |k,v| v.include?(date) } ? { |k,v| "Today is #{k}'s Birthday!"}.join(" ") : ""
+  end
+end
 
+birthday = Birthday.new
+birthday.todays_birthdays
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,19 @@
+require 'birthday_list.rb'
 
+describe 'Birthday list' do 
+  it 'stores a list of birthdays' do
+    birthday = Birthday.new
+    expect(birthday.birthday_list).to eq Mary: "May 31st, 1993",
+    Mitchell: "June 15th, 1993",
+    Kyle: "Jan 27th, 1986", 
+    Sally: "March 17th, 1966"
+  end
+  it 'returns the birthday list one per line in a tidy format' do
+    birthday = Birthday.new
+    expect(birthday.display_birthdays).to eq ("Mary, May 31st, 1993\nMitchell, June 15th, 1993\nKyle, Jan 27th, 1986\nSally, March 17th, 1966\n")
+    end
+  it 'notifies user of people with birthdays today' do
+    birthday = Birthday.new
+    expect(birthday.todays_birthdays).to eq "Today is Sally's Birthday!"
+    end
+  end
